5c1f6fc5e3271637f332393535c3e80e65608740
justdheja <justdheja@gmail.com>
2019-06-04 09:40:26 +0700

    up

--- script.js
+++ script.js
@@ -58,19 +58,19 @@ class Calculator {
 
     getDisplayNumber(number) {
         const stringNumber = number.toString()
-        const integerNumber = parseFloat(stringNumber.split('.')[0])
-        const decimalDigits = stringNumber.split('.')[1]
-        let integerDisplay
-        if (isNaN(integerDigits)) {
-            integerDisplay = ''
-        } else {
-            integerDisplay = integerDigits.toLocaleString('en', {maximumFractionDigits: 0} )
-        }
-        if (decimalDigits != null) {
-            return `${integerDisplay} ${decimalDigits}`
-        }else{
-            return integerDisplay
-        }
+    const integerDigits = parseFloat(stringNumber.split('.')[0])
+    const decimalDigits = stringNumber.split('.')[1]
+    let integerDisplay
+    if (isNaN(integerDigits)) {
+      integerDisplay = ''
+    } else {
+      integerDisplay = integerDigits.toLocaleString('en', { maximumFractionDigits: 0 })
+    }
+    if (decimalDigits != null) {
+      return `${integerDisplay}.${decimalDigits}`
+    } else {
+      return integerDisplay
+    }
     }
 
     updateDisplay() {
